02f52b1b65846787ec091b89a175848071a0d228
KVetal <kvetal@mail.ru>
2020-05-17 23:48:30 +0500

    Add description for task 3

--- 3/main.cpp
+++ 3/main.cpp
@@ -10,7 +10,7 @@ void print_vector(std::vector<int> v){
 }
 int main()
 {
-	std::vector<int> v1 {1,2,3,4,1,6,33,88,2,8,34,8};
+	std::vector<int> v1 {1,1,2,3,4,1,6,33,88,45,45,2,8,34,8,9,9};
 	print_vector(v1);
 	for (int i = 0; i < v1.size()-1;++i)
 		for(int j = i+1;j <v1.size();++j)
