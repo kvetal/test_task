ed660d2e432c80588777a355b9be834d6e5dde90
KVetal <kvetal@mail.ru>
2020-05-17 23:10:41 +0500

    Add task 3

---
+++ 3/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+void print_vector(std::vector<int> v){
+	for(int t:v)
+	{
+		std::cout << t << '\t';
+	}
+	std::cout << '\n';
+}
+int main()
+{
+	std::vector<int> v1 {1,2,3,4,1,6,33,88,2,8,34,8};
+	print_vector(v1);
+	for (int i = 0; i < v1.size()-1;++i)
+		for(int j = i+1;j <v1.size();++j)
+		{		
+			if (v1[i] == v1[j])
+				v1.erase(v1.begin()+j);
+		}
+	print_vector(v1);
+}
